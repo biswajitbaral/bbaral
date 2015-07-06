3d2a003b4386a249afbf4a9412b6fb3ca21afbf4
biswajitbaral <biswajit.baral@gmail.com>
2015-07-06 14:07:11 +0530

    The Coin Change Problem
    The Coin Change Problem
    https://www.hackerrank.com/challenges/coin-change

---
+++ src/dp/CoinChangingProWithRepetion.java
@@ -0,0 +1,121 @@
+package dp;
+
+/*
+ * 
+ * https://www.hackerrank.com/challenges/coin-change
+ * 
+ How many different ways can you make change for an amount, given a list of coins? In this problem, your code will need to efficiently compute the answer.
+
+ Task
+
+ Write a program that, given
+
+ An amount N and types of infinite available coins M
+ A list of M coins - C={C1,C2,C3,..,CM}
+ Prints out how many different ways you can make change from the coins to STDOUT.
+
+ The problem can be formally stated:
+
+ Given a value N, if we want to make change for N cents, and we have infinite supply of each of C={C1,C2,…,CM} valued coins, how many ways can we make the change? The order of coins doesn’t matter.
+
+ Constraints
+
+ 1≤Ci≤50
+ 1≤N≤250
+ 1≤M≤50
+ The list of coins will contain distinct integers.
+ Solving the overlapping subproblems using dynamic programming
+
+ You can solve this problem recursively, but not all the tests will passs unless you optimise your solution to eliminate the overlapping subproblems using a dynamic programming solution
+
+ Or more specifically;
+
+ If you can think of a way to store the checked solutions, then this store can be used to avoid checking the same solution again and again.
+ Input Format
+
+ First line will contain 2 integer N and M respectively. 
+ Second line contain M integer that represent list of distinct coins that are available in infinite amount.
+
+ Output Format
+
+ One integer which is the number of ways in which we can get a sum of N from the given infinite supply of M types of coins.
+
+ Sample Input
+
+ 4 3
+ 1 2 3 
+ Sample Output
+
+ 4
+ Sample Input #02
+
+ 10 4
+ 2 5 3 6
+ Sample Output #02
+
+ 5
+ Explanation
+
+ Example 1: For N=4 and C={1,2,3} there are four solutions: {1,1,1,1},{1,1,2},{2,2},{1,3}
+
+ Example 2: For N=10 and C={2,5,3,6} there are five solutions: {2,2,2,2,2},{2,2,3,3},{2,2,6},{2,3,5},{5,5}.
+
+ Hints
+
+ Think about the degenerate cases:
+
+ How many ways can you give change for 0 cents?
+ How many ways can you give change for >0 cents, if you have no coins?
+ If you are having trouble defining your solutions store, then think about it in terms of the base case (n=0)
+ For help on reading from STDIN, see the HackerRank environment help page under the "Sample Problem Statement" section.
+ */
+
+public class CoinChangingProWithRepetion
+{
+
+    /**
+     * @param args
+     */
+    public static long CoinChange(int[] coins, int sum)
+    {
+        int noCoin = coins.length;
+
+        long[][] data = new long[noCoin][sum + 1];
+
+        for (int i = 0; i < data.length; i++)
+        {
+            data[i][0] = 1;
+            for (int j = 1; j <= sum; j++)
+            {
+                if (i == 0)
+                {
+                    data[i][j] = (j % coins[i]) == 0 ? 1 : 0;
+                }
+                else
+                {
+                    if (coins[i] > j)
+                    {
+                        data[i][j] = data[i - 1][j];
+                    }
+                    else
+                    {
+                        data[i][j] = data[i - 1][j] + data[i][j - coins[i]];
+                    }
+                }
+
+            }
+
+        }
+        return data[noCoin - 1][sum];
+    }
+
+    public static void main(String[] args)
+    {
+        int[] coins =
+        { 16, 30, 9, 17, 40, 13, 42, 5, 25, 49, 7, 23, 1, 44, 4, 11, 33, 12, 27, 2, 38, 24, 28, 32, 14, 50 };
+        int sum = 245;
+
+        System.out.println(CoinChange(coins, sum));
+    }
+
+}
